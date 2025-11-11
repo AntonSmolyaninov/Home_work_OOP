ec1c78a05b0956c5651e19eeb2048be376d91c1a
Anton <klip07@rambler.ru>
2025-11-12 01:12:00 +1100

    add test

--- src/lawngrass.py
+++ src/lawngrass.py
@@ -1,15 +1,18 @@
 from src.product import Product
 
+
 class LawnGrass(Product):
 
-    def __init__(self,
-                 name: str,
-                 description: str,
-                 price: float,
-                 quantity: int,
-                 country: str,
-                 germination_period: str,
-                 color: str,) -> None:
+    def __init__(
+        self,
+        name: str,
+        description: str,
+        price: float,
+        quantity: int,
+        country: str,
+        germination_period: str,
+        color: str,
+    ) -> None:
         super().__init__(name, description, price, quantity)
         self.country = country  # страна - производитель
         self.germination_period = germination_period  # срок проростания
@@ -19,12 +22,3 @@ class LawnGrass(Product):
         if type(other) is LawnGrass:
             return (self.price * self.quantity) + (other.price * other.quantity)
         raise TypeError
-
-
-
-
-
-
-
-
-
--- src/smartphone.py
+++ src/smartphone.py
@@ -1,17 +1,20 @@
 from src.product import Product
 
+
 class Smartphone(Product):
-    def __init__(self,
-                 name: str,
-                 description: str,
-                 price: float,
-                 quantity: int,
-                 efficiency: float,
-                 model: str,
-                 memory: int,
-                 color: str) -> None:
+    def __init__(
+        self,
+        name: str,
+        description: str,
+        price: float,
+        quantity: int,
+        efficiency: float,
+        model: str,
+        memory: int,
+        color: str,
+    ) -> None:
         super().__init__(name, description, price, quantity)
-        self.efficiency = efficiency   # производительность
+        self.efficiency = efficiency  # производительность
         self.model = model  # модель
         self.memory = memory  # объем встроенной памяти
         self.color = color  # цвет
